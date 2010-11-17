ef1be6d48fe021213302c09e2f6634fb4e19e620
damicoac <damicoac@gmail.com>
2010-11-16 22:45:20 -0500

    finished interface for object source code. corrected file structure

---
+++ src/lab3_source/ObjectFileSource.java
@@ -0,0 +1,119 @@
+package lab3_source;
+
+public class ObjectFileSource implements ObjectFileSourceInterface {
+
+	@Override
+	public String getEntryNameFromLinkingAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAddressInHexFromLinkingAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTypeFromLinkingAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAddressInHexFromTextAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDebugCodeFromTextAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDataWordFromTextAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAdjustmentsFromTextAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTypeFromTextAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLabelReferenceFromTextAtLine(int lineNumberOfObjectFile) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTotalRecordsFromEnd() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getModuleNameFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProgramLengthInHexFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProgramLoadAddressInHexfromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDateFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTimeFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNumberOfLinkingRecoredsFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getNumberOfTextRecordsFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getExecutionStartAddressFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getProgramNameFromHeader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
