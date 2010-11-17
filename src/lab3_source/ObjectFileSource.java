20db02c4b3bf5cc8fa24f4b7c07ab5786132afbe
damicoac <damicoac@gmail.com>
2010-11-17 11:36:11 -0500

    import code for object file finished

--- src/lab3_source/ObjectFileSource.java
+++ src/lab3_source/ObjectFileSource.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class ObjectFileSource implements ObjectFileSourceInterface {
 
@@ -13,8 +14,13 @@ public class ObjectFileSource implements ObjectFileSourceInterface {
 	/*
 	 * this is the data structure that will have all header and end records modeled over it
 	 */
-	private ArrayList<String[]> headerEndRecords = new ArrayList<String[]>();
-
+	private	String[] header = new String [8];
+	
+	/*
+	 * this is the data structure for the end record
+	 */
+	private	String[] endRecord = new String [1];
+	
 	/*
 	 * this is the data structure that will have all text records modeled over it
 	 */
@@ -40,10 +46,11 @@ public class ObjectFileSource implements ObjectFileSourceInterface {
 	{
 		System.out.println("Importing object file : " + objectFileName);
 		
-		//clear data structure
-		headerEndRecords.clear();
+		//clear data structure, not really needed and im not sure that "= null" will work
+		header = null;
 		textRecords.clear();
 		linkingRecords.clear();
+		endRecord = null;
 		
 		//get input from file, set up initial variables 
 		BufferedReader input = new BufferedReader(new FileReader(objectFileName));
@@ -51,36 +58,156 @@ public class ObjectFileSource implements ObjectFileSourceInterface {
 		int numberOfLinkingRecords  = 0;
 		int numberOfTextRecords = 0;
 		
-		//get the header record TODO
+/////////////////////////////////////////////////////////////////////////
+//get the header record
+/////////////////////////////////////////////////////////////////////////
+
+		//get the header file from the object file
+		newLine = input.readLine();
+			
+		//tokenize it
+		StringTokenizer headerTokens = new StringTokenizer (newLine, "|");
 		
+		//skip the 'H"
+		headerTokens.nextToken();
 		
+		//get the module name
+		header[0] = headerTokens.nextToken();
+		
+		//get program length
+		header[1] = headerTokens.nextToken();
+		
+		//get assembler assigned program load address in hex
+		header[2] = headerTokens.nextToken();
+		
+		//get date
+		header[3] = headerTokens.nextToken();
+		
+		//get time
+		header[4] = headerTokens.nextToken();
+		
+		//get number of linking records
+		header[5] = headerTokens.nextToken();
+		
+		//set number of linking records
+		numberOfLinkingRecords = Integer.parseInt(header[5]);
+		
+		//get number of text records
+		header[6] = headerTokens.nextToken();
+		
+		//set number of text records
+		numberOfTextRecords = Integer.parseInt(header[6]);
+		
+		//get execution start address
+		header[7] = headerTokens.nextToken();
+		
+/////////////////////////////////////////////////////////////////////////
+//get the text record		
+/////////////////////////////////////////////////////////////////////////
+
 		//keep getting lines of from the file and add them to the properties objects until the file and been completely traversed
 		//do this for text files
 		int i = 0;
-		while (i < numberOfLinkingRecords)
-		{
-			
-			//TODO
-			
-			numberOfLinkingRecords++;
-		}
-		
-		
-		//keep getting lines of from the file and add them to the properties objects until the file and been completely traversed
-		//do this for linking files
-		i = 0;
 		while (i < numberOfTextRecords)
 		{
-		
+			//get the header file from the object file
+			newLine = input.readLine();
 			
-			//TODO
+			//tokenize it
+			StringTokenizer textTokens = new StringTokenizer (newLine, "|");
+						
+			//make an array to add those tokens to
+			String[] text = new String [textTokens.countTokens() -1];
+			
+			//skip "T"
+			text[0] = textTokens.nextToken();
+			
+			//get address in hex
+			text[1] = textTokens.nextToken();
+			
+			//get debug code
+			text[2] = textTokens.nextToken();
+			
+			//get data word in hex
+			text[3] = textTokens.nextToken();
+			
+			//get number of adjustments
+			text[4] = textTokens.nextToken();
+			
+			//get type
+			text[5] = textTokens.nextToken();
+			
+			//if there are tokens left because of a label, if greater than 1 then the next token must be a label reference
+			if (textTokens.countTokens() > 1)
+			{
+				//get label reference if it exists
+				text[0] = textTokens.nextToken();
+							
+			}
+			
+			//TODO how many of the action, label, or type tokens can we have in object file??????? Currently do not get this data
+
+			//add the array to the list of text Records
+			textRecords.add(text);
+			
+			//get next text record
 			numberOfTextRecords++;
 		}
 		
-		//get end record TODO
+/////////////////////////////////////////////////////////////////////////
+//get the linking record
+/////////////////////////////////////////////////////////////////////////
+
+		//keep getting lines of from the file and add them to the properties objects until the file and been completely traversed
+		//do this for linking files
+		i = 0;
+		while (i < numberOfLinkingRecords)
+		{
 		
+			//get the header file from the object file
+			newLine = input.readLine();
+			
+			//tokenize it
+			StringTokenizer linkingTokens = new StringTokenizer (newLine, "|");
+			
+			//make an array to add those tokens to
+			String[] linking = new String [3];
+			
+			//skip "L"
+			linkingTokens.nextToken();
+			
+			//get entry name
+			linking[0] = linkingTokens.nextToken();
+			
+			//get address in hex
+			linking[1] = linkingTokens.nextToken();
+			
+			//get the type
+			linking[2] = linkingTokens.nextToken();
+			
+			//add the array to the list of text Records
+			linkingRecords.add(linking);
+			
+			//get next linking record
+			numberOfLinkingRecords++;
+		}
+
+/////////////////////////////////////////////////////////////////////////
+//get the end record
+/////////////////////////////////////////////////////////////////////////		
 		
+		//get end record 
+		//get the end record from the object file
+		newLine = input.readLine();
 		
+		//tokenize it
+		StringTokenizer endTokens = new StringTokenizer (newLine, "|");
+		
+		//skip the 'E'
+		endTokens.nextToken();
+		
+		//get the total number of records
+		endRecord [0] = endTokens.nextToken();
 		
 		
 		//close the input
@@ -89,6 +216,8 @@ public class ObjectFileSource implements ObjectFileSourceInterface {
 		
 	}
 	
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
 	
 	@Override
 	public String getEntryNameFromLinkingAtLine(int lineNumberOfObjectFile) {
